1b639dae27db9225f7d18b8a7036b56aadc30340
cccampb2 <calebccampbell19@gmail.com>
2025-02-07 20:48:53 -0500

    refactor: refactor modal components

--- src/components/DeleteItemCardModal/DeleteItemCardModal.jsx
+++ src/components/DeleteItemCardModal/DeleteItemCardModal.jsx
@@ -1,3 +1,4 @@
+import { Modal } from "../Modal/Modal";
 import "./DeleteItemCardModal.css";
 
 function DeleteItemCardModal({
@@ -8,25 +9,22 @@ function DeleteItemCardModal({
   isLoading,
 }) {
   return (
-    <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
-      <div className="modal__content modal__content_type_delete-item-card">
-        <button
-          onClick={onExitButtonClick}
-          type="button"
-          className="modal__close-btn modal__close-btn_type_delete-item-card"
-        ></button>
-        <p className="modal__question">
-          Are you sure you want to delete this item?
-        </p>
-        <p className="modal__statement">This action is irreversible.</p>
-        <button onClick={handleCardDelete} className="modal__delete-btn">
-          {isLoading ? "Deleting..." : "Yes, delete item"}
-        </button>
-        <button onClick={handleCancelDelete} className="modal__cancel-btn">
-          Cancel
-        </button>
-      </div>
-    </div>
+    <Modal
+      name={"delete-item-card"}
+      isOpen={isOpen}
+      onClose={onExitButtonClick}
+    >
+      <p className="modal__question">
+        Are you sure you want to delete this item?
+      </p>
+      <p className="modal__statement">This action is irreversible.</p>
+      <button onClick={handleCardDelete} className="modal__delete-btn">
+        {isLoading ? "Deleting..." : "Yes, delete item"}
+      </button>
+      <button onClick={handleCancelDelete} className="modal__cancel-btn">
+        Cancel
+      </button>
+    </Modal>
   );
 }
 
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import "./Modal.css";
+
+export const Modal = ({ name, onClose, children, isOpen }) => {
+  // here is `useEffect` for the `Escape` listener
+  useEffect(() => {
+    // we should define the handler inside `useEffect`, so that it wouldn’t lose the reference to be able to remove it
+    const handleEscape = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+    // don’t forget to remove the listener in the `clean-up` function
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, [onClose]);
+
+  // here is the overlay handler
+  const handleOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className={`modal modal_type_${name} ${isOpen ? "modal_opened" : ""}`}
+      onClick={handleOverlay}
+    >
+      {/* the container for the contents */}
+      <div className={`modal__content modal__content_type_${name}`}>
+        {children}
+        <button
+          className={`modal__close-btn modal__close-btn_type_${name}`}
+          type="button"
+          onClick={onClose}
+        />
+      </div>
+    </div>
+  );
+};
